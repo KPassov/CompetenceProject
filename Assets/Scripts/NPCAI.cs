3e1e087b11c71eb9fbf9390e50ed21a7e1218cbd
Kasper Passov <pvx884@alumni.ku.dk>
2015-10-15 11:17:23 +0200

    game!

---
+++ Assets/Scripts/NPCAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCAI : MonoBehaviour {
+
+    NavMeshAgent navAgent;
+    GameObject player;
+
+	void Awake () {
+    	navAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+    void Update(){
+        navAgent.destination = player.transform.position; 
+    }
+}
