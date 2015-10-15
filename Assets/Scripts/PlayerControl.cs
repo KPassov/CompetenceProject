be22392c2ae0bf7548f17db8add7492f88bc1ec7
Kasper Passov <pvx884@alumni.ku.dk>
2015-10-15 10:51:47 +0200

    broken movement

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    Rigidbody rb;
+    public float thrust = 500f;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	void Update () {
+        if(Input.GetButton("down")){
+            rb.AddForce(Vector3.backwards * thrust);
+        } 
+        if(Input.GetButton("up")){
+            rb.AddForce(Vector3.forwards * thrust);
+        }  
+        if(Input.GetButton("left")){
+            rb.AddForce(Vector3.left * thrust);    
+        }  
+        if(Input.GetButton("right")){
+            rb.AddForce(Vector3.right * thrust);    
+        }
+	}
+}
