6cbaac245cf36d57fa2379c8982aea47a708d3d9
Tudor Dragan <me@tudordev.com>
2015-10-15 14:35:04 +0200

    removed duplicate pragmas

--- Assets/Scripts/InvisibilityPowerUp.cs
+++ Assets/Scripts/InvisibilityPowerUp.cs
@@ -6,11 +6,9 @@ public class InvisibilityPowerUp : GenericPowerUp {
 
 	#pragma warning disable 0114
 	// Use this for initialization
-#pragma warning disable 0114 
 	void Start () {
 		base.Start();
 	}
-#pragma warning restore 0114
 	
 	// Update is called once per frame
 	void Update () {
