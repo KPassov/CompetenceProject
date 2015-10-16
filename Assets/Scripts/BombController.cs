472015c89e682165faf3dd6fb1af263f38eaa11c
Tudor Dragan <me@tudordev.com>
2015-10-16 12:21:06 +0200

    bomb prefab

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -3,13 +3,40 @@ using System.Collections;
 
 public class BombController : MonoBehaviour {
 
+	public float countdownTime = 3.0f;
+	public float pushRadius = 3.0f;
+	public float killRadius = 1.0f;
+
+	private GameObject grenade;
+	private GameObject explosion;
+
 	// Use this for initialization
 	void Start () {
-	
+		foreach(Transform child in transform){
+			if(child.CompareTag("Grenade"))
+				grenade = child.gameObject;
+			if(child.CompareTag("Explosion"))
+				explosion = child.gameObject;
+		}
+		grenade.SetActive(true);
+		explosion.SetActive(false);
+		StartCoroutine(StartCountDown());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	IEnumerator StartCountDown(){
+		yield return new WaitForSeconds(countdownTime);
+		Explode();
+	}
+
+	void Explode(){
+		grenade.SetActive(false);
+		explosion.SetActive(true);
+	}
+
+
 }
