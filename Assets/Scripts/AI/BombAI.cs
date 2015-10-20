96033b6d8e4ed747514181574b4ec4e787ea4091
Kasper Passov <kasper.passov@gmail.com>
2015-10-20 11:24:33 +0200

    more bombsite

--- Assets/Scripts/AI/BombAI.cs
+++ Assets/Scripts/AI/BombAI.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class BombAI : GeneralAI {
 
-    Vector3 bombSite;
+    GameObject[] bombSites;
 
     void Start()
     {
-        bombSite = GameObject.FindGameObjectsWithTag("BombSite")[Random.Range(0, 1)].transform.position;
-        navAgent.SetDestination(bombSite);
+        bombSites = GameObject.FindGameObjectsWithTag("BombSite");
+        setRandomBombSite();
     }
     
+    void setRandomBombSite()
+    {
+        navAgent.SetDestination(bombSites[Random.Range(0, 2)].transform.position);
+    }
+
     override protected void CloseMove()
     {
-        navAgent.SetDestination(bombSite);
+        setRandomBombSite();
+    }
+
+    protected override void NoClose()
+    {
+        setRandomBombSite();
     }
 
     protected override void Touched()
---
+++ Assets/Scripts/BombSite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombSite : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) { 
+        if(other.gameObject.tag == "NPC" && other.GetComponent<BombAI>() != null)
+        {
+            //print("Bomb has been Planted!");
+            // Plant bomb
+            other.gameObject.SetActive(false);
+            GameObject.Instantiate(Resources.Load("Prefabs/NPCs/FleeingNPC"), other.transform.position, other.transform.rotation);
+        }
+    }
+}
